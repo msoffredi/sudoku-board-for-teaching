6bc4281fdd41557a4f9ed41dd56fb08f8fd980e7
Marcelo Soffredi <msoffredi@gmail.com>
2021-07-26 17:23:16 -0500

    Sudoku component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import { Annotations } from './components/Cell/Cell';
 import './App.scss';
-import { CellGroup, CellGroupValues } from './components/CellGroup/CellGroup';
+import { CellGroupValues } from './components/CellGroup/CellGroup';
+import { Sudoku, SudokuValues } from './components/Sudoku/Sudoku';
 
 function App() {
   const annotations: Annotations = [1, 2, null, null, null, 6, 7, 8, 9];
   const group = [
     1,2,3,4,5,6,7,8,annotations
   ] as CellGroupValues;
+  const groups = [
+    group,
+    group,
+    group,
+    group,
+    group,
+    group,
+    group,
+    group,
+    group,
+  ] as SudokuValues;
 
   return (
     <main>
       <div className="container-center">
-        <CellGroup values={group} />
+        <Sudoku values={groups} />
       </div>
     </main>
   );
