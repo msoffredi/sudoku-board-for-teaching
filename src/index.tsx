3dd8f9dc0a2d212d2bd841239f92df81af5cee83
Marcelo Soffredi <msoffredi@gmail.com>
2021-07-30 16:43:42 -0500

    More test configurations

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,16 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import React from 'react';
 
-const store = createStore(reducers, composeWithDevTools(
-    applyMiddleware(thunk)
-));
+const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
